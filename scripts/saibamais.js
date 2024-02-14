281a7be097291c4659a84648d5349add28ced3c2
Clube da Matematica <150398115+GapClubeMatematica@users.noreply.github.com>
2024-02-14 18:28:13 -0300

    Update saibamais.js

--- scripts/saibamais.js
+++ scripts/saibamais.js
@@ -5,12 +5,9 @@ function initGoogleSheetsApi() {
     discoveryDocs: ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
   }).then(function() {
     loadProfessoresFromGoogleSheet();
-    loadBolsistasFromGoogleSheet();
-
+    loadBolsistasFromGoogleSheet()
   });
 }
-
-// Função para carregar os dados dos professores da planilha
 function loadProfessoresFromGoogleSheet() {
   const spreadsheetId = '1bnIVpHL_md8u_XXo-zA3ZJGbA2J_ijj0XtlJJjOPzvk'; 
   const sheetName = 'professores';
@@ -24,31 +21,48 @@ function loadProfessoresFromGoogleSheet() {
     if (data.length > 0) {
       const carouselInner = document.querySelector('#carouselProfessores .carousel-inner');
 
-      data.forEach(function(row, index) {
-        const [nome, area, formacao, imagem] = row;
+      // Loop para percorrer os dados e criar os itens do carrossel
+      for (let i = 0; i < data.length; i += 2) {
+        const row1 = data[i];
+        const row2 = data[i + 1];
 
         const carouselItem = document.createElement('div');
-        carouselItem.className = 'carousel-item' + (index === 0 ? ' active' : '');
+        carouselItem.className = 'carousel-item' + (i === 0 ? ' active' : '');
 
-        const cardHtml = `
-          <div class="row">
-            <div class="col-md-6">
-              <div class="card text-center border-0"> 
-                <img src="${imagem}" class="card-img-top rounded-circle mx-auto mt-3" alt="${nome}" style="max-width: 180px; max-height: 180px;">
-                <div class="card-body d-flex flex-column justify-content-center align-items-center">
-                  <h5 class="card-title">${nome}</h5>
-                  <p class="card-text">Área: ${area}</p>
-                  <p class="card-text">Formação: ${formacao}</p>
-              
+        
+        if (row1) {
+          const [nome1, area1, formacao1, imagem1] = row1;
+          const [nome2, area2, formacao2, imagem2] = row2 || []; 
+
+          const cardHtml = `
+            <div class="row">
+              <div class="col-md-6">
+                <div class="card text-center border-0"> 
+                  <img src="${imagem1}" class="card-img-top rounded-circle mx-auto mt-3" alt="${nome1}" style="max-width: 180px; max-height: 180px;">
+                  <div class="card-body d-flex flex-column justify-content-center align-items-center">
+                    <h5 class="card-title">${nome1}</h5>
+                    <p class="card-text">Área: ${area1}</p>
+                    <p class="card-text">Formação: ${formacao1}</p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="card text-center border-0"> 
+                  <img src="${imagem2}" class="card-img-top rounded-circle mx-auto mt-3" alt="${nome2}" style="max-width: 180px; max-height: 180px;">
+                  <div class="card-body d-flex flex-column justify-content-center align-items-center">
+                    <h5 class="card-title">${nome2}</h5>
+                    <p class="card-text">Área: ${area2 || ''}</p>
+                    <p class="card-text">Formação: ${formacao2 || ''}</p>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        `;
+          `;
 
-        carouselItem.innerHTML = cardHtml;
-        carouselInner.appendChild(carouselItem);
-      });
+          carouselItem.innerHTML = cardHtml;
+          carouselInner.appendChild(carouselItem);
+        }
+      }
     }
   });
 }
@@ -86,6 +100,7 @@ function loadBolsistasFromGoogleSheet() {
   });
 }
 
+
 function createBolsistaCard(dataRow) {
   if (dataRow) {
     const [nome, curso, periodo, imagem] = dataRow;
@@ -106,5 +121,4 @@ function createBolsistaCard(dataRow) {
     return ''; 
   }
 }
-
 gapi.load('client', initGoogleSheetsApi);
